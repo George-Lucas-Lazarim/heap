b6f26c7450d19c18e5c82470cb06a0dacac134da
George Lucas Lazarim <georgelucas1402@gmail.com>
2026-06-04 14:29:21 -0300

    parent leftchild rightchild and swap functions

--- heap.c
+++ heap.c
@@ -1,1 +1,33 @@
 #include "heap.h"
+
+struct maxHeap* initHeap (int capacity) {
+    if (capacity <= 0) {
+        printf("\nError! Invalid capacity");
+        return NULL;
+    }
+
+    struct maxHeap* heap = (struct maxHeap*) malloc (sizeof(struct maxHeap));
+
+    if (heap == NULL) {
+        printf("\nError! Memory allocation failed for heap");
+        return NULL;
+    }
+
+    heap->size = 0;
+    heap->capacity = capacity;
+    heap->array = (int*) malloc (capacity * sizeof(int));
+
+    return heap;
+}
+
+int parent (int i) { return ((i - 1) / 2); }
+
+int leftChild (int i) { return ((i * 2) + 1); }
+
+int rightChild (int i) { return ((i * 2) + 2); }
+
+void swap (int *x, int *y) {
+    int aux = *x;
+    *x = *y;
+    *y = aux;
+}
--- heap.h
+++ heap.h
@@ -1,6 +1,19 @@
 #ifndef HEAP_H
 #define HEAP_H
 
+#include <stdio.h>
+#include <stdlib.h>
 
+struct maxHeap {
+    int *array;
+    int capacity;
+    int size;
+};
+
+struct maxHeap* initHeap (int capacity);
+int parent (int i);
+int leftChild (int i);
+int rightChild (int i);
+void swap (int *x, int *y);
 
 #endif
