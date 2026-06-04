74d1493c58ceffd1b38a3ef3f787db5d4e5f8030
George Lucas Lazarim <georgelucas1402@gmail.com>
2026-06-04 15:15:43 -0300

    main

--- simpleheap.c
+++ simpleheap.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 #include "heap.h"
+
+#define SIZE 5
+
+int main() {
+    struct maxHeap* heap = initHeap(SIZE);
+
+    int aux;
+
+    for (int i = 0; i < SIZE; i++) {
+        printf("\nEnter an integer to add to the heap: ");
+        scanf("%d", &aux);
+        insert(heap, aux);
+        printf("Number: %d added successfully!", aux);
+        printHeap(heap);
+        printf("\n\n");
+    }
+
+    free(heap->array);
+    free(heap);
+}
